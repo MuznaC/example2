b29538d375132bc3009a82dee2cf2af08eeb9ced
MuznaC <71279994+MuznaC@users.noreply.github.com>
2024-11-15 14:51:38 -0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+
+document.getElementById('hamburger').addEventListener('click',function(){
+
+    let nav_menu = document.querySelector('.nav-links');
+    nav_menu.classList.toggle('show');
+
+});
